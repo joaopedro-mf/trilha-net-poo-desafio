6671a63d6e68148d36a382503fad7c1a00d70ef2
João Pedro <joaopedrom98@gmail.com>
2022-10-18 10:07:49 -0300

    Subindo desafio

---
+++ Models/Nokia.cs
@@ -0,0 +1,15 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp) =>
+            Console.WriteLine("instalando Aplicativo...");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+var celularNokia = new Nokia("9912-5003","Nokia", "123123123", 64 );
+
+var celularIphone = new Iphone("9912-5003", "Iphone 13", "123123123", 64);
+
+celularNokia.ReceberLigacao();
+
+celularIphone.InstalarAplicativo("teste");
